3d2c87ca248067f26878fa6ef24593274b3e4476
NelsonNobrega <91686870+NelsonNobrega@users.noreply.github.com>
2021-12-01 15:09:19 -0300

    Add files via upload

---
+++ Web Firebase Cadastro/firebase.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+	apiKey: "AIzaSyCxjVmLSWOP3pRUEsD5JdpGJFSQ0t0HDcY",
+    authDomain: "pwii-b9ed2.firebaseapp.com",
+    projectId: "pwii-b9ed2",
+    storageBucket: "pwii-b9ed2.appspot.com",
+    messagingSenderId: "304532465854",
+    appId: "1:304532465854:web:64b5601566c01ee15516ef"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
